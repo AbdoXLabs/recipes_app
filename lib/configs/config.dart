f24e191d4fd62ceb1be7ff806eedaf8ffef61a83
Abdenbi <saher.fes@gmail.com>
2019-04-30 16:38:14 +0200

    update

--- lib/configs/config.dart
+++ lib/configs/config.dart
@@ -5,17 +5,17 @@ class Config {
   static final String terms_url = 'http://www.google.com';
   static final String about_us_url = 'http://www.google.com';
 
-  static final String appName = 'Recipes App';
+  static final String appName = 'Recipes App'; // name of the app
 
-  static final String iosAppId = "";
-  static final String androidAppId = "";
+  static final String iosAppId = "ma.yalabs.recipesApp";
+  static final String androidAppId = "ma.yalabs.recipes_app";
 
-  static final String admobAppID = 'ca-app-pub-8630423092275079~2611649187';
+  static final String admobAppID = 'ca-app-pub-2670899878941270~1391084475'; // app id => admobe
 
-//  static final String bannerID = 'ca-app-pub-8630423092275079/7071614852';
-//  static final String interstitial = 'ca-app-pub-8630423092275079/4024926917';
-static final String bannerID = BannerAd.testAdUnitId;
-  static final String interstitial = InterstitialAd.testAdUnitId;
+  static final String bannerID = 'ca-app-pub-2670899878941270/2129451077'; // banner id => admob
+  static final String interstitial = 'ca-app-pub-2670899878941270/3606184279'; // interstitial is => admob
+//  static final String bannerID = BannerAd.testAdUnitId;
+//  static final String interstitial = InterstitialAd.testAdUnitId;
 
   static bool personalizedAds = true;
 }
